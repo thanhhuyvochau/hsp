d499a2f48308cdac46c09ae3aa9de6735909d8bd
HieuZozo <lebaminhhieuyh@gmail.com>
2023-09-26 23:34:48 +0700

    Initial Commit

---
+++ src/test/java/com/example/hbs/HbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
