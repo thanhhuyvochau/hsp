b0aa66a4ce5f4f9901d9a252ac1f594abee2ac3f
Hieu Do <hieudthe153215@fpt.edu.vn>
2023-10-24 12:26:17 +0700

    Add and update UserProfile

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -55,3 +55,53 @@ var swiper = new Swiper(".our-partner", {
 
     },
 });
+
+//Hidden password
+function password_show_hide(number) {
+    if (number == 1) {
+        var x = document.getElementById("password1");
+        var show_eye = document.getElementById("show_eye1");
+        var hide_eye = document.getElementById("hide_eye1");
+        hide_eye.classList.remove("d-none");
+        if (x.type === "password") {
+            x.type = "text";
+            show_eye.style.display = "none";
+            hide_eye.style.display = "block";
+        } else {
+            x.type = "password";
+            show_eye.style.display = "block";
+            hide_eye.style.display = "none";
+        }
+    } else
+        if (number == 2) {
+            var x = document.getElementById("password2");
+            var show_eye = document.getElementById("show_eye2");
+            var hide_eye = document.getElementById("hide_eye2");
+            hide_eye.classList.remove("d-none");
+            if (x.type === "password") {
+                x.type = "text";
+                show_eye.style.display = "none";
+                hide_eye.style.display = "block";
+            } else {
+                x.type = "password";
+                show_eye.style.display = "block";
+                hide_eye.style.display = "none";
+            }
+        }
+        else {
+            var x = document.getElementById("password3");
+            var show_eye = document.getElementById("show_eye3");
+            var hide_eye = document.getElementById("hide_eye3");
+            hide_eye.classList.remove("d-none");
+            if (x.type === "password") {
+                x.type = "text";
+                show_eye.style.display = "none";
+                hide_eye.style.display = "block";
+            } else {
+                x.type = "password";
+                show_eye.style.display = "block";
+                hide_eye.style.display = "none";
+            }
+        }
+
+}
