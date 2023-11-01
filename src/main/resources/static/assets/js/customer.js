d780b997bf9ea6b8d6019f9453f841b013329a43
Ori <khoannhe153156@fpt.edu.vn>
2023-11-01 21:14:13 +0700

    update js history-booking, create-booking

--- src/main/resources/static/assets/js/customer.js
+++ src/main/resources/static/assets/js/customer.js
@@ -7,35 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-//filter status
-document.addEventListener("DOMContentLoaded", function () {
-    // Lắng nghe sự kiện click cho tất cả các phần tử có class 'dropdown-item'
-    document.querySelectorAll('.dropdown-item').forEach(item => {
-        item.addEventListener('click', filterTable);
-    });
-});
-
-function filterTable(event) {
-    const selectedStatus = event.target.getAttribute('data-value');
-    const dropdownToggle = document.querySelector('.btn-secondary');
-    dropdownToggle.innerText = event.target.innerText;
-
-    // Get all rows in the table
-    const rows = document.querySelectorAll("table tbody tr");
-
-    // Loop through the rows and hide/show based on the selected status
-    rows.forEach(row => {
-        const statusCell = row.querySelector("td:nth-child(8)"); // Assuming "Trạng thái" is in the 8th column
-
-        if (selectedStatus === "all" || statusCell.textContent === selectedStatus) {
-            row.style.display = "table-row";
-        } else {
-            row.style.display = "none";
-        }
-    });
-}
-
-
 document.addEventListener("DOMContentLoaded", function () {
     const submitButton = document.querySelector("button[type='submit']");
 
@@ -60,3 +31,150 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', filterTable);
+});
+    const table = document.getElementById("myTable");
+    const tbody = table.querySelector("tbody");
+    const rows = tbody.getElementsByTagName("tr");
+    const rowsPerPage = 4;
+    let currentPage = 1;
+    let currentStatusFilter = "all";
+    // Tạo các nút phân trang
+    function createPagination() {
+        const totalPages = Math.ceil(rows.length / rowsPerPage);
+        const pagination = document.getElementById("pagination");
+        pagination.innerHTML = '';
+
+        for (let i = 1; i <= totalPages; i++) {
+            const li = document.createElement("li");
+            li.classList.add("page-item");
+
+            const a = document.createElement("a");
+            a.classList.add("page-link");
+            a.href = "#";
+            a.textContent = i;
+            a.setAttribute("data-page", i);
+
+            a.addEventListener("click", function () {
+                currentPage = parseInt(this.getAttribute("data-page"), 10);
+                displayPage(currentPage);
+            });
+
+            li.appendChild(a);
+            pagination.appendChild(li);
+        }
+    }
+
+    // Hiển thị trang hiện tại
+    function displayPage(pageNumber) {
+    currentPage = pageNumber;
+    const startIndex = (pageNumber - 1) * rowsPerPage;
+    const endIndex = startIndex + rowsPerPage;
+
+    for (let i = 0; i < rows.length; i++) {
+        const statusCell = rows[i].querySelector("td:nth-child(8"); // Assuming "Trạng thái" is in the 8th column
+        const statusValue = statusCell.textContent.trim();
+
+        if (currentStatusFilter === "all" || statusValue === currentStatusFilter) {
+            if (i >= startIndex && i < endIndex) {
+                rows[i].style.display = "table-row";
+            } else {
+                rows[i].style.display = "none";
+            }
+        } else {
+            rows[i].style.display = "none";
+        }
+    }
+}
+
+// Lọc bảng theo trạng thái được chọn
+function filterTable(event) {
+    const selectedStatus = event.target.getAttribute('data-value')
+    currentStatusFilter = selectedStatus;
+
+    for (let i = 0; i < rows.length; i++) {
+        const statusCell = rows[i].querySelector("td:nth-child(8"); // Assuming "Trạng thái" is in the 8th column
+        const statusValue = statusCell.textContent.trim();
+
+        if (selectedStatus === "all" || statusValue === selectedStatus) {
+            rows[i].classList.remove("hidden");
+        } else {
+            rows[i].classList.add("hidden");
+        }
+    }
+
+    // Tính lại tổng số trang dựa trên số hàng đã filter
+    const visibleRows = Array.from(tbody.querySelectorAll("tr:not(.hidden)"));
+    createPagination(visibleRows);
+    displayPage(1);
+}
+
+// Bắt đầu bằng việc hiển thị trang đầu tiên khi trang web được tải
+createPagination(rows);
+displayPage(1);
+
+// Lọc bảng theo trạng thái được chọn
+function filterTable(event) {
+    const selectedStatus = event.target.getAttribute('data-value')
+    currentStatusFilter = selectedStatus;
+
+    for (let i = 0; i < rows.length; i++) {
+        const statusCell = rows[i].querySelector("td:nth-child(8"); // Assuming "Trạng thái" is in the 8th column
+        const statusValue = statusCell.textContent.trim();
+
+        if (selectedStatus === "all" || statusValue === selectedStatus) {
+            rows[i].classList.remove("hidden");
+        } else {
+            rows[i].classList.add("hidden");
+        }
+    }
+
+    // Tính lại tổng số trang dựa trên số hàng đã filter
+    const visibleRows = Array.from(tbody.querySelectorAll("tr:not(.hidden)"));
+    createPagination(visibleRows);
+    displayPage(1);
+}
+
+// Bắt đầu bằng việc hiển thị trang đầu tiên khi trang web được tải
+createPagination(rows);
+displayPage(1);
+
+    // Lọc bảng theo trạng thái được chọn
+    function filterTable(event) {
+        const selectedStatus = event.target.getAttribute('data-value')
+        currentStatusFilter = selectedStatus;
+
+        for (let i = 0; i < rows.length; i++) {
+            const statusCell = rows[i].querySelector("td:nth-child(8)"); // Assuming "Trạng thái" is in the 8th column
+            const statusValue = statusCell.textContent.trim();
+
+            if (selectedStatus === "all" || statusValue === selectedStatus) {
+                rows[i].classList.remove("hidden");
+            } else {
+                rows[i].classList.add("hidden");
+            }
+        }
+
+        createPagination();
+        displayPage(1);
+    }
+
+    // Lắng nghe sự kiện click cho tất cả các phần tử có class 'dropdown-item'
+    document.querySelectorAll('.dropdown-item').forEach(item => {
+        item.addEventListener('click', function (event) {
+            const selectedStatus = event.target.getAttribute('data-value');
+            const dropdownToggle = document.querySelector('.btn-secondary');
+            dropdownToggle.innerText = event.target.innerText;
+
+            filterTable(selectedStatus);
+        });
+    });
+
+    // Bắt đầu bằng việc hiển thị trang đầu tiên khi trang web được tải
+    createPagination();
+    displayPage(1);
+});
+
