198afaf586ff09ffa48bffac810ef30cd163be07
Le Ba Minh Hieu K15HL <hieulbmhe151429@fpt.edu.vn>
2023-11-01 09:36:50 +0700

    edit login, search room
    Signed-off-by: Le Ba Minh Hieu K15HL <hieulbmhe151429@fpt.edu.vn>

--- src/main/java/fu/hbs/repository/CategoryRoomPriceRepository.java
+++ src/main/java/fu/hbs/repository/CategoryRoomPriceRepository.java
@@ -1,13 +1,21 @@
 package fu.hbs.repository;
 
+import fu.hbs.dto.CategoryRoomPriceDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import fu.hbs.entities.CategoryRoomPrice;
+
+import java.util.List;
 
 @Repository
 public interface CategoryRoomPriceRepository extends JpaRepository<CategoryRoomPrice, Long> {
     CategoryRoomPrice findByRoomCategoryId(Long id);
 
     CategoryRoomPrice findByRoomPriceId(Long id);
+
+    @Query(value = "SELECT cr.* FROM category_room_price cr WHERE room_category_id = ?1 AND price_type = 'Weekday'", nativeQuery = true)
+    List<CategoryRoomPriceDTO> getAllCategoryRoomPrice(Long id);
+
 }
