12705a0665145558f6f4945a8e53cd2176557e91
Ori <khoannhe153156@fpt.edu.vn>
2023-11-08 03:02:27 +0700

    update frontend createBooking.html errorBooking.html confirm-refund.html, add frontend invoice.html, update js customer.js

--- src/main/resources/static/assets/js/manager.js
+++ src/main/resources/static/assets/js/manager.js
@@ -1,3 +1,9 @@
 $(document).ready( function () {
     $('#myTable').DataTable();
 } );
+
+//popover
+document.addEventListener("DOMContentLoaded", function () {
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+});
