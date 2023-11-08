dc21665f4f1305847f4ec85f471ef92562e6cb01
Hieu Do <hieudthe153215@fpt.edu.vn>
2023-11-09 00:15:04 +0700

    update frontend

--- src/main/resources/static/assets/js/customer.js
+++ src/main/resources/static/assets/js/customer.js
@@ -1,4 +1,6 @@
-
+$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
 //popover
 document.addEventListener("DOMContentLoaded", function () {
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
@@ -31,65 +33,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
-$(document).ready(function () {
-    // Setup - add a text input to each footer cell
-    $('#theadtr')
-        .clone(true)
-        .addClass('filters')
-        .appendTo('#thead');
-
-    var table = $('#myTable').DataTable({
-        orderCellsTop: true,
-        fixedHeader: true,
-        initComplete: function () {
-            var api = this.api();
-
-            // For each column
-            api
-                .columns()
-                .eq(0)
-                .each(function (colIdx) {
-                    // Set the header cell to contain the input element
-                    var cell = $('.filters th').eq(
-                        $(api.column(colIdx).header()).index()
-                    );
-                    var title = $(cell).text();
-                    $(cell).html('<input type="text" placeholder="' + title + '" />');
-
-                    // On every keypress in this input
-                    $(
-                        'input',
-                        $('.filters th').eq($(api.column(colIdx).header()).index())
-                    )
-                        .off('keyup change')
-                        .on('change', function (e) {
-                            // Get the search value
-                            $(this).attr('title', $(this).val());
-                            var regexr = '({search})'; //$(this).parents('th').find('select').val();
-
-                            var cursorPosition = this.selectionStart;
-                            // Search the column for that value
-                            api
-                                .column(colIdx)
-                                .search(
-                                    this.value != ''
-                                        ? regexr.replace('{search}', '(((' + this.value + ')))')
-                                        : '',
-                                    this.value != '',
-                                    this.value == ''
-                                )
-                                .draw();
-                        })
-                        .on('keyup', function (e) {
-                            e.stopPropagation();
-
-                            $(this).trigger('change');
-                            $(this)
-                                .focus()[0]
-                                .setSelectionRange(cursorPosition, cursorPosition);
-                        });
-                });
-        },
-    });
-});
