a9fe87f736413f59b78d89e53566977698330075
Thanh Huy <huyvct@hiworld.com.vn>
2023-12-02 23:32:56 +0700

    add validBooking property for HotelBooking

--- src/main/java/fu/hbs/entities/HotelBooking.java
+++ src/main/java/fu/hbs/entities/HotelBooking.java
@@ -54,4 +54,5 @@ public class HotelBooking {
     private BigDecimal depositPrice = BigDecimal.ZERO;
     private Instant checkIn;
     private Instant checkOut;
+    private Boolean validBooking;
 }
